82da4b6b66a28c2556a744fd07cffb325d5071e0
Matthew Gleich <matthewgleich@gmail.com>
2019-11-10 13:14:34 -0500

    Reboot at end of script

--- Install/Kubernetes/install.sh
+++ Install/Kubernetes/install.sh
@@ -23,3 +23,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt-get update -q && \
 sudo apt-get install -qy kubeadm
+
+echo "Pi will reboot in 5 seconds"
+sleep 5
+sudo reboot
