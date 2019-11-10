80dced3ada15cb6a7ffff0af8ccc3e0492e033ac
Matthew Gleich <matthewgleich@gmail.com>
2019-11-09 23:00:55 -0500

    Create Kubernetes/install.sh

---
+++ Install/Kubernetes/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Installing Kubernetes onto a RPI
+
+# Little Warning:
+echo "If this script fails because of download, try rebooting and running again."
+echo "If that doesn't work then just run the script again every couple of minutes"
+
+# Install Docker
+curl -sSL get.docker.com | sh && \
+sudo usermod pi -aG docker
+
+# Disable Swap
+sudo dphys-swapfile swapoff && \
+sudo dphys-swapfile uninstall && \
+sudo update-rc.d dphys-swapfile remove
+echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
+sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
+echo $orig | sudo tee /boot/cmdline.txt
+
+# Add repo list and install kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update -q && \
+sudo apt-get install -qy kubeadm
