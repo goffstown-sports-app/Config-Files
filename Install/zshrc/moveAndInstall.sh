6358ecc39e58bc13f4345d2d90f53625ac0e4a44
Matthew Gleich <matthewgleich@gmail.com>
2019-11-10 15:02:09 -0500

    Create moveAndInstall.sh

---
+++ Install/zshrc/moveAndInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will download all oh-my-zsh packages and move the .zshrc file to the one at root
+# This script assumes that this Config-Files repo is in the ~/Documents/Github folder
+
+# Installing Packages
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Moving .zshrc
+rm ~/.zshrc
+cp ~/Documents/Github/Conifg-Files/Install/zshrc/.zshrc ~/.zshrc
